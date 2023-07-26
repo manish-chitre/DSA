b0de3f582e192c07a47ebd16615148d5c69aca70
Manish Chitre <manish_chitre@hotmail.com>
2023-07-26 14:23:43 +0530

    added a string file

--- Linear Data Structures/strings.js
+++ Linear Data Structures/strings.js
@@ -1,13 +1,67 @@
-function removeVowels(str) {
-  let newStr = "";
-  let ovels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]; //0(1)
-  for (let i = 0; i < str.length; i++) {
-    //o(n)
-    if (!ovels.includes(str[i])) {
-      newStr += str[i];
-    }
+// function removeVowels(str) {
+//   let newStr = "";
+//   let ovels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]; //0(1)
+//   for (let i = 0; i < str.length; i++) {
+//     //o(n)
+//     if (!ovels.includes(str[i])) {
+//       newStr += str[i];
+//     }
+//   }
+//   return newStr;
+// }
+
+// console.log(removeVowels("manish"));
+
+// var reverseStr = function (s, k) {
+//   let sArr = s.split("");
+//   for (let i = 0; i < sArr.length; i += 2 * k) {
+//     if (i + k - 1 < sArr.length) {
+//       reverse(i, i + k - 1, sArr);
+//     } else {
+//       reverse(i, sArr.length, sArr);
+//     }
+//   }
+
+//   let newS = sArr.join("");
+
+//   return newS;
+// };
+
+// function reverse(start, end, sArr) {
+//   while (start < end) {
+//     let temp = sArr[start];
+//     sArr[start] = sArr[end];
+//     sArr[end] = temp;
+//     start++;
+//     end--;
+//   }
+// }
+
+// console.log(reverseStr("abcd", 2));
+
+var reverseWords = function (s) {
+  let wordsArr = s.split(" ");
+  for (let i = 0; i < wordsArr.length; i++) {
+    reverse(wordsArr[i], i, wordsArr);
   }
-  return newStr;
+
+  let modifiedWords = wordsArr.join(" ");
+  return modifiedWords;
+};
+
+function reverse(s, i, wordsArr) {
+  let sArr = s.split("");
+  let start = 0;
+  let end = sArr.length - 1;
+  while (start < end) {
+    let temp = sArr[start];
+    sArr[start] = sArr[end];
+    sArr[end] = temp;
+    start++;
+    end--;
+  }
+
+  wordsArr[i] = sArr.join("");
 }
 
-console.log(removeVowels("manish"));
+console.log(reverseWords("Let's take LeetCode contest"));
